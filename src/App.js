8830ddfdc8b0aee777e3679a1012eb5c749628be
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-05-07 17:46:54 +0530

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 import './App.css';
+ import NavBar from "./Components/NavBar";
+
 
 function App() {
   return (
-    <div className="app">
-      
+    <div className="w-full h-auto bg-bodyColor text-lightText">
+      <div className="container max-w-screen-2xl mx-auto px-16">
+        <NavBar/> 
+      </div>
     </div>
+
   );
 }
 
