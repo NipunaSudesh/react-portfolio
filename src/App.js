532b7aaff65c0b938880b336e2d2cbcd25e2c166
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-05-09 15:00:55 +0530

    updates navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 import './App.css';
  import NavBar from "./Components/NavBar";
-
+ import Home from "./Components/Home";
+import AboutUs from "./Components/AboutUs";
 
 function App() {
   return (
     <div className="w-full h-auto bg-bodyColor text-lightText">
       <div className="container max-w-screen-2xl mx-auto px-16">
         <NavBar/> 
+        <Home/>
+        <AboutUs/>
       </div>
     </div>
 
---
+++ src/Components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+  <section id="Home" className='w-full h-[800px] pb-20 flex border-b-[1px] border-b-black'>
+    Home
+
+  </section>
+  )
+}
