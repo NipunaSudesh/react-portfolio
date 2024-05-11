9c5badcc57815d0a7be14ebb0f92b731aaf762e3
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-05-11 09:25:16 +0530

    created services component

--- src/Components/Services/MyServices.js
+++ src/Components/Services/MyServices.js
@@ -1,12 +1,34 @@
 import React from 'react'
+import ServicesCard from './ServicesCard'
+import { FaFacebook, FaMobile } from 'react-icons/fa'
 
 export default function MyServices() {
   return (
-    <section id="MyServices" className='w-full h-[800px] pb-20 flex border-b-[2px] border-b-black'>
+    <section id="MyServices" className='w-full pb-20  border-b-[2px] border-b-black'>
+      <div  className='flex flex-col gap-14'>
     <div>
-      <h1>My Services</h1>
+      <h1 className='flex justify-center items-center mt-10 text-6xl font-bold text-white'>My Services</h1>
     </div>
+    <div className='mt-14 grid grid-cols-3 gap-20'>
+    <ServicesCard 
+      title="App Development"
+      des="Lorem Ipsum text in a React application, you can either use a Lorem Ipsum generator library or create a simple function to generate "
+      icon={<FaFacebook/>}
+      />
+    <ServicesCard 
+      title="App Development"
+      des="Lorem Ipsum text in a React application, you can either use a Lorem Ipsum generator library or create a simple function to generate Lorem Ipsum text. Here's an example of a simple function that generates Lorem Ipsum text"
+      icon={<FaMobile/>}
+      />
+    <ServicesCard 
+      title="App Development"
+      des="Lorem Ipsum text in a React application, you can either use a Lorem Ipsum generator library or create a simple function to generate Lorem Ipsum text. Here's an example of a simple function that generates Lorem Ipsum text"
+      icon={<FaMobile/>}
+      />
 
+
+    </div>
+    </div>
   </section>
   )
 }
--- src/Components/Services/ServicesCard.js
+++ src/Components/Services/ServicesCard.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import { FaArrowRight } from "react-icons/fa";
 
-export default function ServicesCard() {
+export default function ServicesCard({title,des,icon}) {
   return (
-    <div>ServicesCard</div>
+    <div className='w-full px-12 h-auto py-10 rounded-1g shadow-shadowOne flex flex-col
+    bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b
+    hover: from-black, hover:to-[#1e2024] transition-colors duration-100 group'>
+
+     <div className='flex h-77  overflow-y-hidden '>
+
+    <div className='flex h-full flex-col gap-6 translate-y-10 group-hover:translate-y-0 transform-opacity duration-500'>
+    <div>
+        <span className='text-5xl text-designColor'>{icon}</span>
+       </div>
+       <div className='flex flex-col gap-6'>
+       <h2 className='text-white font-normal'>{title}</h2>
+       <p className='text-base text-sm tracking-wide mt-3 hover:text-gray-100 duration-300'>{des}</p>
+       <span className='text-2xl text-designColor'>
+        <FaArrowRight/>
+       </span>
+       </div>
+     </div>
+    </div>
+    </div>
   )
 }
