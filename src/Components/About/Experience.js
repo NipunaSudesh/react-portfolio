591da27ed6a786e857aad68a8b68ad2e56906d97
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-05-12 09:20:21 +0530

    created Experience part

---
+++ src/Components/About/Experience.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AboutDetails from './AboutDetails'
+
+export default function Experience() {
+  return (
+    <div class="container mx-auto  ">
+      <div class="w-full grid grid-cols-2 gap-2">
+        <AboutDetails title="2021-Current"
+        des="undergraduate student at university of kelaniya"
+        />
+        </div>
+      </div>
+  )
+}
