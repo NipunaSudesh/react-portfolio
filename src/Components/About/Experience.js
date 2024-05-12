ac1e0ff53fac95bf28b3797a22d10fe790a40599
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-05-12 10:54:48 +0530

    added framer-motion

--- src/Components/About/Experience.js
+++ src/Components/About/Experience.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import AboutDetails from './AboutDetails'
+import {motion} from 'framer-motion';
 
 export default function Experience() {
   return (
-    <div class="container mx-auto  ">
-      <div class="w-full grid grid-cols-2 gap-2">
+    <motion.div initial={{opacity:0}} animate={{opacity:1 , transition:{duration:.5}}} className="container mx-auto  ">
+      <div className="w-full grid grid-cols-2 gap-2">
         <AboutDetails title="2021-Current"
         des="undergraduate student at university of kelaniya"
         />
         </div>
-      </div>
+      </motion.div>
   )
 }
--- src/Components/About/Skill.js
+++ src/Components/About/Skill.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import AboutDetails from './AboutDetails'
+import {motion} from 'framer-motion';
 
 export default function Skill() {
   return (
-    <div class="container mx-auto  ">
-      <div class="w-full grid grid-cols-2 gap-2">
+    <motion.div initial={{opacity:0}} animate={{opacity:1 , transition:{duration:.5}}} className="container mx-auto  ">
+      <div className="w-full grid grid-cols-2 gap-2">
       <AboutDetails title="UI/UX" des="Designing web/App interface"/>
       <AboutDetails title="web Development" des="Web App development"/>
       <AboutDetails title="App Development" des="App Development"/>
       <AboutDetails title="UI/UX" des="Designing web/App interface"/>
         </div>
-      </div>
+      </motion.div>
   )
 }
