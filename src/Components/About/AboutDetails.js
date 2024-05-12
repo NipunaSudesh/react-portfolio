d6f1859ad626b4cd2a01664297697cca30077a33
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-05-12 08:54:35 +0530

    created education skill part

--- src/Components/About/AboutDetails.js
+++ src/Components/About/AboutDetails.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
-export default function AboutDetails() {
+export default function AboutDetails({title,des}) {
   return (
-    <div>AboutDetails</div>
+    <div className='flex flex-col justify-center items-center bg-gray-200 rounded-lg shadow-md'>
+        <h2 className='text-xl font-bold'>{title}</h2>
+        <p className='text-gray-700 text-base'> {des}</p>
+    </div>
   )
 }
--- src/Components/About/AboutUs.js
+++ src/Components/About/AboutUs.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { my } from '../../assets'
+import AboutDetails from './AboutDetails';
 
 export default function AboutUs() {
   return (
     <section id="AboutUs" className='w-full h-[750px] pt-20 pb-20 flex border-b-[2px] border-b-black'>
 
     <div className='w-1/3 '>
-      <img className='bject-cover w-full h-full rounded-[5%]' alt='myProfile' src={my}/>
+      <img className='object-cover w-full h-full rounded-[5%]' alt='myProfile' src={my}/>
     </div>
     <div className='w-2/3 bg-dark p-10'>
     <div className='flex flex-col gap-7'>
@@ -14,11 +15,19 @@ export default function AboutUs() {
       <p className='text-l tracking-wide mt-3 hover:text-gray-100 duration-300'>I'm Nipuna Sudesh, a passionate and driven Software Engineering undergraduate student at the University of Kelaniya. I welcome you to explore my portfolio, where I showcase my journey, skills, and projects in the world of software development.</p>
       </div>
       <div>
-        <ul className='w-full grid grid-cols-4'>
+        <ul className='w-full grid grid-cols-4 gap-4 text-2xl text-white'>
           <li className='aboutList'>Education</li>
           <li className='aboutList'>Professional Skills</li>
           <li className='aboutList'>Experience</li>
         </ul>
+      </div>
+      <div class="container mx-auto px-2 py-2 ">
+      <div class="flex flex-col gap-2">
+      <AboutDetails title="UI/UX" des="Designing web/App interface"/>
+      <AboutDetails title="web Development" des="Web App development"/>
+      <AboutDetails title="App Development" des="App Development"/>
+      <AboutDetails title="UI/UX" des="Designing web/App interface"/>
+        </div>
       </div>
     </div>
 
