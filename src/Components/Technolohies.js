4ff00428f4f1036bcaf91c13ab7fa2b8e8c84e1d
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-07-25 15:18:29 +0530

    added animation to tech

--- src/Components/Technolohies.js
+++ src/Components/Technolohies.js
@@ -34,98 +34,103 @@ export default function Technolohies() {
     <div>
       <h1 className='flex justify-center items-center mt-10 text-6xl font-bold text-white'>Technologies</h1>
     </div>
-    <div className='flex flex-wrap items-center justify-center gap-4 '>
-        <motion.div 
+    <motion.div
+          whileInView={{opacity:1,x:0}}
+          initial={{opacity:0,x:-100}}
+          transition={{duration:2}}
+    className='flex flex-wrap items-center justify-center gap-4 '>
+    <motion.div 
         variants={iconVariants(2.5)}
         initial="initial"
         animate="animate"
         className='border-neutral-800 p-4 rounded-2xl border-4'>
-            <FaReact className='text-6xl text-cyan-400'/>
-        </motion.div>
-        <motion.div 
+          <FaReact className='text-6xl text-cyan-400'/>
+      </motion.div>
+      <motion.div 
+        variants={iconVariants(3)}
+        initial="initial"
+        animate="animate"
+        className='border-neutral-800 p-4 rounded-2xl border-4'>
+          <FaNode className='text-6xl text-green-500'/>
+      </motion.div>
+      <motion.div
+        variants={iconVariants(3.5)}
+        initial="initial"
+        animate="animate"
+        className='border-neutral-800 p-4 rounded-2xl border-4'>
+          <SiExpress className='text-6xl text-white'/>
+      </motion.div>
+      <motion.div 
+        variants={iconVariants(4)}
+        initial="initial"
+        animate="animate"
+        className='border-neutral-800 p-4 rounded-2xl border-4'>
+          <SiMongodb className='text-6xl text-green-400'/>
+      </motion.div>
+      <motion.div
+        variants={iconVariants(4.5)}
+        initial="initial"
+        animate="animate"
+        className='border-neutral-800 p-4 rounded-2xl border-4'>
+          <FaJava className='text-6xl text-cyan-400'/>
+      </motion.div>
+      <motion.div 
+        variants={iconVariants(2)}
+        initial="initial"
+        animate="animate"
+        className='border-neutral-800 p-4 rounded-2xl border-4'>
+          <FaPhp className='text-6xl text-blue-400'/>
+      </motion.div>
+      <motion.div 
         variants={iconVariants(2.5)}
         initial="initial"
         animate="animate"
         className='border-neutral-800 p-4 rounded-2xl border-4'>
-            <FaNode className='text-6xl text-green-500'/>
-        </motion.div>
-        <motion.div
-        variants={iconVariants(2.5)}
+          <FaHtml5 className='text-6xl text-cyan-400'/>
+      </motion.div>
+      <motion.div
+        variants={iconVariants(3)}
         initial="initial"
         animate="animate"
         className='border-neutral-800 p-4 rounded-2xl border-4'>
-            <SiExpress className='text-6xl text-white'/>
-        </motion.div>
-        <motion.div 
-        variants={iconVariants(2.5)}
+          <FaCss3 className='text-6xl text-orange-400'/>
+      </motion.div>
+      <motion.div
+        variants={iconVariants(3.5)}
         initial="initial"
         animate="animate"
         className='border-neutral-800 p-4 rounded-2xl border-4'>
-            <SiMongodb className='text-6xl text-green-400'/>
-        </motion.div>
-        <motion.div
-        variants={iconVariants(2.5)}
+          <AiOutlineJavaScript className='text-6xl text-black bg-yellow-300'/>
+      </motion.div>
+      <motion.div 
+        variants={iconVariants(4)}
         initial="initial"
         animate="animate"
         className='border-neutral-800 p-4 rounded-2xl border-4'>
-            <FaJava className='text-6xl text-cyan-400'/>
-        </motion.div>
-        <motion.div 
-        variants={iconVariants(2.5)}
+          <FaBootstrap className='text-6xl text-red-400'/>
+      </motion.div>
+      <motion.div
+        variants={iconVariants(2)}
         initial="initial"
         animate="animate"
         className='border-neutral-800 p-4 rounded-2xl border-4'>
-            <FaPhp className='text-6xl text-blue-400'/>
-        </motion.div>
-        <motion.div
-        
+          <RiTailwindCssFill className='text-6xl text-cyan-300 bg-slate-200'/>
+      </motion.div>
+      <motion.div
         variants={iconVariants(2.5)}
         initial="initial"
-        animate="animate"className='border-neutral-800 p-4 rounded-2xl border-4'>
-            <FaHtml5 className='text-6xl text-cyan-400'/>
-        </motion.div>
-        <motion.div
-        variants={iconVariants(2.5)}
+        animate="animate" 
+        className='border-neutral-800 p-4 rounded-2xl border-4'>
+          <SiMysql className='text-6xl text-white'/>
+      </motion.div>
+      <motion.div
+        variants={iconVariants(3)}
         initial="initial"
         animate="animate"
         className='border-neutral-800 p-4 rounded-2xl border-4'>
-            <FaCss3 className='text-6xl text-orange-400'/>
-        </motion.div>
-        <motion.div
-        variants={iconVariants(2.5)}
-        initial="initial"
-        animate="animate"
-        className='border-neutral-800 p-4 rounded-2xl border-4'>
-            <AiOutlineJavaScript className='text-6xl text-black bg-yellow-300'/>
-        </motion.div>
-        <motion.div
-        
-        variants={iconVariants(2.5)}
-        initial="initial"
-        animate="animate"className='border-neutral-800 p-4 rounded-2xl border-4'>
-            <FaBootstrap className='text-6xl text-red-400'/>
-        </motion.div>
-        <motion.div
-        variants={iconVariants(2.5)}
-        initial="initial"
-        animate="animate"
-        className='border-neutral-800 p-4 rounded-2xl border-4'>
-            <RiTailwindCssFill className='text-6xl text-cyan-300 bg-slate-200'/>
-        </motion.div>
-        <motion.div
-        variants={iconVariants(2.5)}
-        initial="initial"
-        animate="animate"className='border-neutral-800 p-4 rounded-2xl border-4'>
-            <SiMysql className='text-6xl text-white'/>
-        </motion.iv>
-        <motion.div
-        variants={iconVariants(2.5)}
-        initial="initial"
-        animate="animate"
-        className='border-neutral-800 p-4 rounded-2xl border-4'>
-            <BiLogoPostgresql className='text-6xl text-cyan-300'/>
-        </motion.div>
-    </div>
+          <BiLogoPostgresql className='text-6xl text-cyan-300'/>
+      </motion.div>
+    </motion.div>
     </div>
     </section>
   )
