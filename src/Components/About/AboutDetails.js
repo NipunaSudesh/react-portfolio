69213f98ac98be184e7b9557c4740e13755dd9b8
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-07-29 12:08:50 +0530

    updated style

--- src/Components/About/AboutDetails.js
+++ src/Components/About/AboutDetails.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import {motion} from 'framer-motion';
 
-export default function AboutDetails({title,des}) {
+export default function AboutDetails(props) {
   return (
     <div className=' flex flex-col w-auto justify-center items-center bg-gray-200 rounded-lg'>
-        <h2 className='text-xl font-bold'>{title}</h2>
-        <p className='text-gray-700 text-base'> {des}</p>
+        <h2 className='text-xl font-bold'>{props.title}</h2>
+        <p className='text-gray-700 text-base p-1'> {props.des}</p>
     </div>
 
   )
